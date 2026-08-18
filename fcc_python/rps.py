11630f224a0f11aa02a8f107fe6bd70fc54c895d
Anudeep Akella <anudeepakella@gmail.com>
2026-08-18 11:34:14 +0530

    Fix spelling errors and enhance comments in rps.py
    Corrected spelling errors in the game messages and improved code comments.

--- fcc_python/rps.py
+++ fcc_python/rps.py
@@ -1,31 +1,33 @@
-# Rock paper scissors game using python
+# Rock paper scissors game using Python
 
 import random
 
 def get_choices():
+    """ Gets the choices of both the player and the computer. """"
     player_choice = input("Enter your choice (rock,paper,scissors):").lower()
-    options = ["rock","paper","scissors"]
+    options = ["rock", "paper", "scissors"]
     computer_choice = random.choice(options)
     choices = {"player":player_choice,"computer":computer_choice}
     return choices
 
 
 def check_win(player,computer):
+    """ Tells us who won the game. """
     if player == computer:
         return "It's a tie!"
     elif player == "rock":
         if computer == "scissors":
             return "Rock smashes scissors! You win!"
         else:
-            return "Paper covers rock! You loose"
+            return "Paper covers rock! You lose"
     elif player == "scissors":
         if computer == "paper":
-            return "scissors cuts paper! You win!"
+            return "Scissors cuts paper! You win!"
         else:
-            return "Rock smashes scissors! You loose!"
+            return "Rock smashes scissors! You lose!"
         elif player == "paper":
         if computer == "scissors":
-            return "Scissors cuts paper! You loose!"
+            return "Scissors cuts paper! You lose!"
         else:
             return "Paper covers rock! You win!"
 
